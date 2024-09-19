69dff0ceb647ff2cd0b3dc3ec397832cb51a2bf6
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-09-19 20:30:52 +0530

    pre deploy check5

--- gym/settings.py
+++ gym/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'gym.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gym',
-        'USER': 'postgres',
-        'PASSWORD': 'Deepak@15', 
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
